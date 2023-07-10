a21e710ec45854e07242ee83338ea778d1cad2d6
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-07-10 22:06:41 +0100

    add the add book feature

--- js/app.js
+++ js/app.js
@@ -1,20 +1,48 @@
-const books = [
+let books = [
   {
     title: 'The world erth catalog',
     author: 'Stewart Brand'
   }
 ]
 
+const formTag = document.querySelector('.form');
+const titleInput = document.querySelector('.title-tag');
+const authorTag = document.querySelector('.author-tag');
 const booksCollectionTag = document.querySelector('.books-collection');
-const bookCard = document.createElement('div');
-bookCard.classList.add('book');
 
-books.forEach(book => {
-  bookCard.innerHTML = `
-    <div class="title">${book.title}</div>
-    <div class="author">${book.author}</div>
-    <button>Remove</button>
-    <hr>
-  `;
-  booksCollectionTag.appendChild(bookCard);
+const refreshBookCollection = () => {
+  booksCollectionTag.innerHTML = '';
+  books.forEach(book => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book');
+    bookCard.innerHTML = `
+      <div class="title">${book.title}</div>
+      <div class="author">${book.author}</div>
+      <button>Remove</button>
+      <hr>
+    `;
+    booksCollectionTag.appendChild(bookCard);
+    console.log(bookCard);
+  })
+}
+
+// Method to collect the book data from the form
+const collectBook = () => {
+}
+
+// Method to add book to the collection
+const addBook = () => {
+  console.log(titleInput, authorTag);
+  const book = {
+    title: titleInput.value,
+    author: authorTag.value
+  }
+  console.log(book);
+  books.push(book);
+}
+
+formTag.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  addBook();
+  refreshBookCollection();
 })
