6f7963101b3568e86b6c2fc7e13fbc7d84de044b
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-07-13 02:07:21 +0100

    add logic of the menu button interactions

--- js/app.js
+++ js/app.js
@@ -31,9 +31,9 @@ const retrieveBooksFromStorage = () => {
   books = storedBooks ? JSON.parse(storedBooks) : [];
 };
 
-// Function to display all books in the collection
-const displayBooks = () => {
-  const booksContainer = document.querySelector('.books-collection');
+// Function to display the books list section
+const displayBooksList = () => {
+  const booksContainer = document.querySelector('.book-list');
   booksContainer.innerHTML = '';
 
   books.forEach((book) => {
@@ -43,7 +43,7 @@ const displayBooks = () => {
     bookCard.appendChild(titleElement);
 
     const authorElement = document.createElement('span');
-    authorElement.textContent = ` ${book.author}`; // Notice the space before ${book.author}
+    authorElement.textContent = ` ${book.author}`;
     titleElement.appendChild(authorElement);
 
     const removeButton = document.createElement('button');
@@ -56,6 +56,30 @@ const displayBooks = () => {
 
     booksContainer.appendChild(bookCard);
   });
+};
+
+// Function to handle menu button clicks
+const handleMenuButtonClick = (sectionToDisplay) => {
+  const booksListSection = document.querySelector('.books-collection-container');
+  const addBookFormSection = document.querySelector('.new-book-form');
+  const contactInfoSection = document.querySelector('.contact-information');
+
+  if (sectionToDisplay === 'booksList') {
+    // Display the books list section and hide the others
+    booksListSection.style.display = 'block';
+    addBookFormSection.style.display = 'none';
+    contactInfoSection.style.display = 'none';
+  } else if (sectionToDisplay === 'addBookForm') {
+    // Display the add book form section and hide the others
+    booksListSection.style.display = 'none';
+    addBookFormSection.style.display = 'block';
+    contactInfoSection.style.display = 'none';
+  } else if (sectionToDisplay === 'contactInfo') {
+    // Display the contact info section and hide the others
+    booksListSection.style.display = 'none';
+    addBookFormSection.style.display = 'none';
+    contactInfoSection.style.display = 'block';
+  }
 };
 
 // Form submit event handler
@@ -72,14 +96,45 @@ form.addEventListener('submit', (e) => {
   const book = new Book(title, author);
   book.addBook();
 
-  displayBooks();
-
   titleInput.value = '';
   authorInput.value = '';
+
+  // Reset the display of all sections except the "List" section
+  const booksListSection = document.querySelector('.books-collection-container');
+  const addBookFormSection = document.querySelector('.new-book-form');
+  const contactInfoSection = document.querySelector('.contact-information');
+
+  booksListSection.style.display = 'block';
+  addBookFormSection.style.display = 'none';
+  contactInfoSection.style.display = 'none';
+
+  displayBooksList();
 });
 
 // Retrieve books collection from localStorage on page load
 retrieveBooksFromStorage();
 
-// Initial display of books
-displayBooks();
+// Initial display of books list section
+displayBooksList();
+
+// Navigation menu buttons event listeners
+const listButton = document.querySelector('.list');
+const newBookButton = document.querySelector('.new-book');
+const contactButton = document.querySelector('.contact');
+
+listButton.addEventListener('click', () => {
+  handleMenuButtonClick('booksList');
+});
+
+newBookButton.addEventListener('click', () => {
+  handleMenuButtonClick('addBookForm');
+});
+
+contactButton.addEventListener('click', () => {
+  handleMenuButtonClick('contactInfo');
+});
+
+// Default display on page load
+document.addEventListener('DOMContentLoaded', () => {
+  handleMenuButtonClick('booksList');
+});
